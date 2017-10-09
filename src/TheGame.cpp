09dac5d7c257db2b8258368c90fe2261d1451abf
josecpujol <josecpujol@gmail.com>
2017-10-08 22:45:13 -0700

    Added main loop function for emscripten

--- src/TheGame.cpp
+++ src/TheGame.cpp
@@ -11,7 +11,6 @@
 #include "Stats.h"
 
 
-
 TheGame::TheGame() {
   window_ = std::make_unique<OpenGlWindow>();
 }
@@ -30,7 +29,7 @@ TheGame::~TheGame() {
 
 bool TheGame::init() {
   
-  if (SDL_InitSubSystem(SDL_INIT_GAMECONTROLLER) != 0) {
+  if (SDL_InitSubSystem(SDL_INIT_JOYSTICK) != 0) {
     LOG_ERROR("Could not initialize gamecontroller");
     return false;
   }
@@ -38,14 +37,10 @@ bool TheGame::init() {
   LOG_INFO("#joysticks found: " << num_joysticks);
   for (int i = 0; i < num_joysticks; i++) {
     LOG_DEBUG("Joystick " << i << ": " << SDL_JoystickNameForIndex(i));
-   /* if (SDL_IsGameController(i)) {
-      LOG_DEBUG("Joystick " << i << " is supported by the game controller interface");
-    } else {
-      LOG_DEBUG("Joystick " << i << " is NOT supported by the game controller interface");
-    }*/
     joystick_ = SDL_JoystickOpen(i);
     if (joystick_) {
       LOG_INFO("Joystick found: " << SDL_JoystickName(joystick_));
+      break;
     } else {
       LOG_ERROR("Could not open joystick. Error: "  << SDL_GetError());
     }
@@ -54,6 +49,12 @@ bool TheGame::init() {
   // Text
   if (TTF_Init() < 0) {
     LOG_ERROR("Could not initialize text");
+    return false;
+  }
+
+  // Sound
+  if (SDL_InitSubSystem(SDL_INIT_AUDIO) != 0) {
+    LOG_ERROR("Could not initialize audio");
     return false;
   }
   
@@ -77,11 +78,11 @@ bool TheGame::init() {
   return true;
 }
 
-void TheGame::processEvents(std::vector<SDL_Event>& events, bool *done) {
+void TheGame::processEvents(const std::vector<SDL_Event>& events) {
   for (auto& e : events) {
     // Process events
     if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
-      *done = true;
+      done_ = true;
     }
  /*   if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_z) stage_->incAmbient(0.1f);
     if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_x) stage_->incAmbient(-0.1f);
@@ -94,11 +95,73 @@ void TheGame::processEvents(std::vector<SDL_Event>& events, bool *done) {
       ResourcesManager::getInstance().setWindowDimensions(e.window.data1, e.window.data2);
     }
     if (e.type == SDL_JOYBUTTONDOWN) {
-      LOG_DEBUG("Joy button");
+      LOG_DEBUG("Joy button: " << e.jbutton.which << " " << std::to_string(e.jbutton.button));
     }
     if (e.type == SDL_JOYAXISMOTION) {
       LOG_DEBUG("Joy axis");
     }
+    if (e.type == SDL_CONTROLLERBUTTONUP) {
+      LOG_DEBUG("Controller up axis");
+    }
+  }
+}
+
+void TheGame::oneIteration() {
+  Stats& stats = Stats::getInstance();
+  SDL_Event event;
+  Duration time_per_cycle = std::chrono::milliseconds{1000 / target_fps_};
+
+  counter_++;
+  TimePoint start = Clock::now();
+
+  Duration poll_and_process_events_duration = measureFunction([&event, this]{
+    std::vector<SDL_Event> events;
+    TimePoint start = Clock::now();
+    while (SDL_PollEvent(&event) && (Clock::now() - start) < 2ms) {
+      events.push_back(event);  // Get all the events now and process later
+    }
+    processEvents(events);
+  });
+
+  Duration update_duration = measureFunction([this, &time_per_cycle] {
+    stage_->update(
+      SDL_GetKeyboardState(nullptr),
+      (uint32_t)std::chrono::duration_cast<std::chrono::microseconds>(time_per_cycle).count());
+  });
+  stats.time_to_update_ms += toMs(update_duration);
+
+  Duration render_duration = measureFunction([this] {
+    stage_->render();
+    LoggerOpenGl::getInstance().render();
+    glFinish();
+  });
+
+  Duration display_duration = measureFunction([this] {
+    window_->display();
+
+  });
+  stats.time_to_render_ms += toMs(render_duration);
+  stats.frames_rendered++;
+
+  Duration elapsed = (Clock::now() - start);
+  Duration time_left = time_per_cycle - elapsed;
+  /*if (time_left > 0s) {
+  } else {
+  LOG_ERROR_SCREEN("Time for cycle exceeded max. Frame #" << counter
+  << ", time taken: " << toMs(elapsed)
+  << ", time per frame: " << toMs(time_per_cycle)
+  << ", poll_and_process_events: " << toMs(poll_and_process_events_duration)
+  << ", update: " << toMs(update_duration)
+  << ", render: " << toMs(render_duration)
+  << ", display: " << toMs(display_duration));
+  // } */
+  int counter_max = 100;
+  if (counter_ % counter_max == 0) {
+    LOG_SCREEN_N(0, "Fps: " << (1000 * stats.frames_rendered) / toMs(Clock::now() - stats.reset_time));
+    LOG_SCREEN_N(1, "Avg triangles rendered: " << stats.num_triangles / counter_max);
+    LOG_SCREEN_N(2, "Avg render time ms: " << stats.time_to_render_ms / counter_max);
+    LOG_SCREEN_N(3, "Avg update time ms: " << stats.time_to_update_ms / counter_max);
+    stats.reset();
   }
 }
 
@@ -107,69 +170,16 @@ void TheGame::runLoop() {
     LOG_ERROR("No stage loaded");
     return;
   }
-  bool done = false;
-  SDL_Event event;
-  int counter = 0;
-  Duration time_per_cycle = std::chrono::milliseconds{1000 / target_fps_};
-  Duration lag = std::chrono::milliseconds{0};
   TimePoint last_update_call;
 
   Stats& stats = Stats::getInstance();
   stats.reset();
 
-  while (!done) {
-    counter++;
-
-    TimePoint start = Clock::now();
-
-    Duration poll_and_process_events_duration = measureFunction([&done, &event, this]{
-     std::vector<SDL_Event> events;
-     TimePoint start = Clock::now();
-     while (SDL_PollEvent(&event) && (Clock::now() - start) < 2ms) {
-       events.push_back(event);  // Get all the events now and process later
-     }
-     processEvents(events, &done);
-    });
-
-    Duration update_duration = measureFunction([this, &time_per_cycle] {
-      stage_->update(
-        SDL_GetKeyboardState(nullptr),
-        (uint32_t)std::chrono::duration_cast<std::chrono::microseconds>(time_per_cycle).count());
-    });
-    stats.time_to_update_ms += toMs(update_duration);
-
-    Duration render_duration = measureFunction([this] {
-      stage_->render();
-      LoggerOpenGl::getInstance().render();
-      glFinish();
-    });
-
-    Duration display_duration = measureFunction([this] {
-      window_->display();
-
-    });
-    stats.time_to_render_ms += toMs(render_duration);
-    stats.frames_rendered++;
-
-    Duration elapsed = (Clock::now() - start);
-    Duration time_left = time_per_cycle - elapsed;
-    /*if (time_left > 0s) {
-    } else { 
-      LOG_ERROR_SCREEN("Time for cycle exceeded max. Frame #" << counter
-        << ", time taken: " << toMs(elapsed)
-        << ", time per frame: " << toMs(time_per_cycle)
-        << ", poll_and_process_events: " << toMs(poll_and_process_events_duration)
-        << ", update: " << toMs(update_duration)
-        << ", render: " << toMs(render_duration)
-        << ", display: " << toMs(display_duration));
-   // } */
-    int counter_max = 100;
-    if (counter % counter_max == 0) {
-      LOG_SCREEN_N(0, "Fps: " << (1000 * stats.frames_rendered) / toMs(Clock::now() - stats.reset_time));
-      LOG_SCREEN_N(1, "Avg triangles rendered: " << stats.num_triangles / counter_max);
-      LOG_SCREEN_N(2, "Avg render time ms: " << stats.time_to_render_ms / counter_max);
-      LOG_SCREEN_N(3, "Avg update time ms: " << stats.time_to_update_ms / counter_max);
-      stats.reset();
-    }
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop(main_loop, 0, 1);
+#else
+  while (!done_) {
+    oneIteration();
   }
+#endif
 }
--- src/TheGame.h
+++ src/TheGame.h
@@ -15,10 +15,15 @@ public:
   void runLoop();
   
 private:
-  void processEvents(std::vector<SDL_Event>& events, bool *done);
+  void oneIteration();
+  void processEvents(const std::vector<SDL_Event>& events);
   std::unique_ptr<OpenGlWindow> window_;  // Need to destroy this before this class' destructor
   std::unique_ptr<Level> stage_;
   SDL_Joystick *joystick_ = nullptr;
   uint32_t target_fps_ = 60;
+
+  // main loop properties: we need to decouple from a single iteration function
+  int counter_ = 0;
+  bool done_ = false;  // main loop exit condition
 };
 
